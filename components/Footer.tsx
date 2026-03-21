6c3d23bd57cb27a0dd3c3d31e5ed699bd3e9e863
charlie2208 <carlosarayasalfate@gmail.com>
2026-03-20 23:40:48 -0300

    add facebook link to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -57,7 +57,7 @@ export default function Footer(){
           </svg>
         </a>
 
-        <a href="https://facebook.com/reflectoram" target="_blank" className="social-link" aria-label="Facebook">
+        <a href="https://web.facebook.com/profile.php?id=61585018786233" target="_blank" className="social-link" aria-label="Facebook">
           <svg viewBox="0 0 24 24"><path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"/></svg>
         </a>
       </div>
