4af6a55eb7ac8cf43a78e49ddca99eefc69f4f49
charlie2208 <carlosarayasalfate@gmail.com>
2026-08-15 10:24:19 -0400

    change some text in hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,10 @@
 export default function Hero(){
     return(
         <section className="hero" id="inicio">
-            <span className="hero-badge-1">Agencia de Marketing Digital</span>
-            <h1>Impulsamos<br />tú <span>negocio</span></h1>
+            <span className="hero-badge-1">Agencia de Marketing Digital en Chile</span>
+            <h1>Haz crecer<br />tu <span>negocio</span></h1>
             <p className="hero-sub">
-      Potenciamos tu empresa con estrategia, creatividad y datos. Nos enfocamos en atraer clientes y hacer crecer tus ventas.
+      Somos una agencia de marketing especializada en emprendimientos, te ayudamos a crecer con estrategia, creatividad y datos. Nos adaptamos a todos los presupuestos.
             </p>
             <div className="hero-actions">
             <a href="#servicios" className="btn-primary">Ver servicios</a>
